af6957c4b33042f1d17f1dca845b8501360ff828
Igor Lopes <claudioigorai@gmail.com>
2023-05-28 16:08:45 -0300

    Criação do botão de reload para iniciar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -16,7 +16,11 @@ function verificaSeOChutePossuiValorValido(chute){
         document.body.innerHTML = `
             <h2>Você acertou</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-        `;
+        
+            <button id="jogar-novamente" class="btn-jogar">Jogar 
+            novamente</button>
+            `;
+            recognition.stop();
     }
     else if(numero < numeroSecreto){
         elementoChute.innerHTML += `
@@ -37,3 +41,9 @@ function chuteForInvalido(numero){
 function numeroForMaiorOuMenorQueOvalorPermitido(numero){
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click',function(e){
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload();
+    }
+})
